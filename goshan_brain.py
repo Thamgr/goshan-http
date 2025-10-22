6cab7c9bd0730c83d8b8e25e5f197dd2d99a9b76
Vasily Vinogradov <vvinogradov2005@gmail.com>
2025-10-22 17:47:03 +0300

    update api

--- goshan_brain.py
+++ goshan_brain.py
@@ -9,6 +9,12 @@ class GoshanBrain:
             cls._instance = super().__new__(cls)
         return cls._instance
 
-    def process(self):
-        return random.randint(0, 1)
+    def predict(self, image, ms_since_click):
+        # TODO: Implement the prediction logic
+        # The logic should be based on the image and the time since the last click
+        # The image is a numpy array of shape (height, width, 3)
+        # The ms_since_click is an integer representing the time since the last click in milliseconds
+        # The timestamp is a datetime object representing the timestamp of the click
+        # The return value should be a integer value: clicks amount
+        return random.randint(0, 5)
 
--- server.py
+++ server.py
@@ -1,5 +1,9 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from goshan_brain import GoshanBrain
+import base64
+import numpy as np
+from PIL import Image
+from io import BytesIO
 
 app = Flask(__name__)
 goshan = GoshanBrain()
@@ -7,7 +11,19 @@ goshan = GoshanBrain()
 
 @app.route('/should-click', methods=['POST'])
 def should_click():
-    status = goshan.process()
+    data = request.get_json()
+    
+    image_base64 = data.get('image')
+    ms_since_click = data.get('ms_since_click')
+    timestamp = data.get('timestamp')
+    
+    # Декодируем base64 в numpy array
+    image_bytes = base64.b64decode(image_base64)
+    image_buffer = BytesIO(image_bytes)
+    image_pil = Image.open(image_buffer)
+    image_array = np.array(image_pil)
+    
+    status = goshan.predict(image_array, ms_since_click)
     return jsonify({'status': status})
 
 
